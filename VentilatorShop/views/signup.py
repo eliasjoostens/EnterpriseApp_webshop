0a0426885dc0280d7ae2ae6ff5fef94c6de26ce7
eliasjoostens <59340987+eliasjoostens@users.noreply.github.com>
2023-01-04 23:13:05 +0100

    password custom validation

--- VentilatorShop/views/signup.py
+++ VentilatorShop/views/signup.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
 from VentilatorShop.models.customer import Customer
 from django.views import View
-
+import re
 
 class Signup(View):
     def get(self, request):
@@ -45,6 +45,7 @@ class Signup(View):
 
     def validateCustomer(self, customer):
         error_message = None
+        regexp = re.compile('[$#@!*]')
         if (not customer.first_name):
             error_message = "Gelieve uw voornaam in te vullen!"
         elif len(customer.first_name) < 3:
@@ -57,8 +58,14 @@ class Signup(View):
             error_message = 'Voer uw telefoonnummer in aub'
         elif len(customer.phone) < 10:
             error_message = 'Telefoonnummer moet bestaan uit 10 cijfers'
-        elif len(customer.password) < 5:
-            error_message = 'Wachtwoord moet uit minstens 5 karakters bestaan'
+        elif len(customer.password) < 9:
+            error_message = 'Wachtwoord moet uit minstens 9 karakters bestaan'
+        elif not (any(i.isdigit() for i in customer.password)):
+            error_message = 'Wachtwoord moet minstens 1 cijfer bevatten'
+        elif not (any(i.isupper() for i in customer.password)):
+            error_message = 'Wachtwoord moet minstens 1 hoofdletter bevatten'
+        elif not (regexp.search(customer.password)):
+            error_message = 'Wachtwoord moet minstens 1 speciaal karakter bevatten ($, #, @, !, *)'
         elif len(customer.email) < 5:
             error_message = 'E-mail moet uit minstens 5 karakters bestaan'
         elif customer.isExists():
